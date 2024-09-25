b613efc2eb0ce319bda4ceaec2d39f8bcea8d1e2
zb656 <145730190+zb656@users.noreply.github.com>
2024-09-25 18:45:45 -0400

    pattern with easy version of nested for loops

--- sketch.js
+++ sketch.js
@@ -1,6 +1,26 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(245, 245, 11);
+
+  fill(0, 0, 0);
+
+  for (let y = 0; y < height; y += 100) {
+    for (let x = 0; x < width; x += 100) {
+      ellipse(x, y, 30);
+    }
+  }
+
+  for (let y = 50; y < height; y += 100) {
+    for (let x = 50; x < width; x += 100) {
+      ellipse(x, y, 30);
+    }
+  }
+
+  for (let y = 0; y < height; y += 50) {
+    for (let x = 0; x < width; x += 50) {
+      ellipse(x, y, 10);
+    }
+  }
 }
 
-function draw() {
-}
+function draw() {}
